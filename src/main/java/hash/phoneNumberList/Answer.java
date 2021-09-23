3e27b218191ef517572de72cd8f27064f9cab652
djung <dudeo4106@gmail.com>
2021-09-24 01:33:10 +0900

    init phone number list

---
+++ src/main/java/hash/phoneNumberList/Answer.java
@@ -0,0 +1,13 @@
+package hash.phoneNumberList;
+
+public class Answer {
+    public static void main(String[] args) {
+        System.out.println("phoneNumberList");
+        System.exit(0);
+    }
+
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        return answer;
+    }
+}
