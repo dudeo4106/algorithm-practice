c77b3934d5fb9658b811fd65df02e08418c02afa
djung <dudeo4106@gmail.com>
2021-09-30 00:12:53 +0900

    add phone number list

--- src/main/java/hash/phoneNumberList/Answer.java
+++ src/main/java/hash/phoneNumberList/Answer.java
@@ -1,13 +1,26 @@
 package hash.phoneNumberList;
 
+import java.util.HashMap;
+
 public class Answer {
     public static void main(String[] args) {
-        System.out.println("phoneNumberList");
+        String[] phoneBook1 = {"119", "97674223", "1195524421"};
+        String[] phoneBook2 = {"123","456","789"};
+        String[] phoneBook3 = {"12","123","1235","567","88"};
+        String[] phoneBook4 = {"12","345","1"};
+        boolean result = solution(phoneBook1);
+        System.out.println("result ----> " + result);
         System.exit(0);
     }
 
-    public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        return answer;
+    public static boolean solution(String[] phoneBook) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String phone : phoneBook) map.put(phone, 1);
+        for (String phone : phoneBook) {
+            for (int j = 1; j < phone.length(); j++) {
+                if (map.containsKey(phone.substring(0, j))) return false;
+            }
+        }
+        return true;
     }
 }
