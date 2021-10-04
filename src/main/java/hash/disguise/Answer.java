519ff65a53f24624eaa936504c040e23fac4620f
djung <dudeo4106@gmail.com>
2021-10-04 23:46:28 +0900

    add disguise

--- src/main/java/hash/disguise/Answer.java
+++ src/main/java/hash/disguise/Answer.java
@@ -14,11 +14,19 @@ public class Answer {
             {"bluesunglasses", "face"},
             {"smoky_makeup", "face"}
         };
+        int result = solution(clothes2);
+        System.out.println("result ---> " + result);
         System.exit(0);
     }
 
     public static int solution(String[][] clothes) {
-        int answer = 0;
-        return answer;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        int answer = 1;
+
+        for (String[] clothe : clothes) map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        for (int value : map.values()) answer *= (value+1);
+
+        return answer-1;
     }
 }
