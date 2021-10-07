452b36e08fb5ac02d9309ee3119f15705facf1c1
djung <dudeo4106@gmail.com>
2021-10-08 00:43:16 +0900

    in-progressing A truck passing through the bridge

--- src/main/java/stack/queue/truck/Answer.java
+++ src/main/java/stack/queue/truck/Answer.java
@@ -4,6 +4,15 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Answer {
+    static class Truck {
+        int weight;
+        int crossing;
+        public Truck(int weight, int crossing) {
+            this.weight = weight;
+            this.crossing = crossing;
+        }
+    }
+
     public static void main(String[] args) {
         int bridge_length1 = 2;
         int weight1 = 10;
@@ -17,13 +26,24 @@ public class Answer {
         int weight3 = 100;
         int[] truck_weights3 = {10,10,10,10,10,10,10,10,10,10};
 
-        int result = solution(bridge_length1, weight1, truck_weights1);
+        int result = solution(bridge_length2, weight2, truck_weights2);
         System.out.println("result ----> " + result);
         System.exit(0);
     }
 
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
+
+        Queue<Truck> queue = new LinkedList<>();
+        for(int truck_weight : truck_weights) queue.add(new Truck(truck_weight, bridge_length)); // init queue
+
+        /*
+        while(!queue.isEmpty()) {
+
+        }
+         */
+
+        answer++;
         return answer;
     }
 }
