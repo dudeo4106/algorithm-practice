2c8bfff91b4ae7e7db2adb554d78a5cd5388b815
djung <dudeo4106@gmail.com>
2021-10-12 23:35:37 +0900

    update stock price

--- src/main/java/stack/queue/stockPrice/Answer.java
+++ src/main/java/stack/queue/stockPrice/Answer.java
@@ -5,24 +5,22 @@ import java.util.Arrays;
 public class Answer {
     public static void main(String[] args) {
         int[] prices = {1, 2, 3, 2, 3};
-
         int[] result = solution(prices);
         System.out.println(Arrays.toString(result));
         System.exit(0);
     }
 
     public static int[] solution(int[] prices) {
-        int[] ans = new int[prices.length];
+        int[] answer = new int[prices.length];
 
         for (int i = 0; i < prices.length; i++) {
             for (int j = i + 1; j < prices.length; j++) {
-                ans[i]++;
-                if (prices[i] > prices[j])
-                    break;
+                answer[i]++;
+                if (prices[i] > prices[j]) break;
 
             }
         }
 
-        return ans;
+        return answer;
     }
 }
