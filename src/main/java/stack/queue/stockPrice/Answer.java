ecf5ec96f6fd99fad09e20968b8228a7de739139
djung <dudeo4106@gmail.com>
2021-10-12 23:34:09 +0900

    add stock price

--- src/main/java/stack/queue/stockPrice/Answer.java
+++ src/main/java/stack/queue/stockPrice/Answer.java
@@ -12,7 +12,17 @@ public class Answer {
     }
 
     public static int[] solution(int[] prices) {
-        int[] answer = {1, 2};
-        return answer;
+        int[] ans = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                ans[i]++;
+                if (prices[i] > prices[j])
+                    break;
+
+            }
+        }
+
+        return ans;
     }
 }
