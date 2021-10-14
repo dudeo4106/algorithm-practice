072c421b32071c3af0c8953a442f30bc0380c472
djung <dudeo4106@gmail.com>
2021-10-14 22:46:00 +0900

    init sort

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ This is a project for practicing algorithm.
 |[printer](./src/main/java/stack/queue/printer)|stack / queue|
 |[A truck passing through the bridge](./src/main/java/stack/queue/truck)|stack / queue|
 |[Stock price](./src/main/java/stack/queue/stockPrice)|stack / queue|
+|[Kth number](./src/main/java/sort/kthNumber)|sort|
 
 # References
 - Question URL: `https://programmers.co.kr/`
---
+++ src/main/java/sort/kthNumber/Answer.java
@@ -0,0 +1,22 @@
+package sort.kthNumber;
+
+import java.util.Arrays;
+
+public class Answer {
+    public static void main(String[] args) {
+        int result = 0;
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {
+            {2, 5, 3},
+            {4, 4, 1},
+            {1, 7, 3},
+        };
+        System.out.println("result ---> " + Arrays.deepToString(commands));
+        System.exit(0);
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        return answer;
+    }
+}
