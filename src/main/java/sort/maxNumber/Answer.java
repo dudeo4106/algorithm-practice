bd09c03f1c539107de535f34f277ff1386d414c9
djung <dudeo4106@gmail.com>
2021-10-16 01:16:20 +0900

    add max number

--- src/main/java/sort/maxNumber/Answer.java
+++ src/main/java/sort/maxNumber/Answer.java
@@ -1,16 +1,40 @@
 package sort.maxNumber;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Answer {
     public static void main(String[] args) {
         int[] array1 = {6, 10, 2};
         int[] array2 = {3, 30, 34, 5, 9};
-        String result = solution(array1);
+        int[] array3 = {3, 330, 445, 3};
+        int[] array4 = {1, 10, 100, 11, 111};
+
+        String result = solution(array3);
         System.out.println("result ---> " + result);
         System.exit(0);
     }
 
     public static String solution(int[] numbers) {
-        String answer = "";
-        return answer;
+        String[] numbersToStrings = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            numbersToStrings[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(numbersToStrings, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return ((b + a).compareTo(a + b));
+            }
+        });
+
+        if(numbersToStrings[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (String str : numbersToStrings) answer.append(str);
+
+        return answer.toString();
     }
 }
