c42c44f2ef6df469e12e591a66eb0ec179d9eed8
djung <dudeo4106@gmail.com>
2021-10-29 00:02:50 +0900

    in-progress find prime number

--- src/main/java/bruteForce/findPrimeNumber/Answer.java
+++ src/main/java/bruteForce/findPrimeNumber/Answer.java
@@ -1,8 +1,10 @@
 package bruteForce.findPrimeNumber;
 
+import java.util.HashSet;
+
 public class Answer {
     public static void main(String[] args) {
-        String numbers = "17";
+        String numbers = "011";
 
         int res = solution(numbers);
 
@@ -11,7 +13,36 @@ public class Answer {
     }
 
     public static int solution(String numbers) {
+        HashSet<Integer> set = new HashSet<>();
+
+        permutation("", numbers, set);
+
         int answer = 0;
+        while(set.iterator().hasNext()){
+            int a = set.iterator().next();
+            set.remove(a);
+            if(a==2) answer++;
+            if(a%2!=0 && isPrime(a)) answer++;
+        }
         return answer;
+    }
+
+    public static void permutation(String prefix, String str, HashSet<Integer> set) {
+        int n = str.length();
+        if(!prefix.equals("")) {
+            set.add(Integer.valueOf(prefix));
+        }
+
+        for(int i=0; i<n; i++) {
+            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n), set);
+        }
+    }
+
+    public static boolean isPrime(int n){
+        if(n==0 || n==1) return false;
+        for(int i=2; i<=(int)Math.sqrt(n); i+=i){
+            if(n%i == 0) return false;
+        }
+        return true;
     }
 }
